ef1f2a29c735465a9c4344ba4dc7a2e17aa65da8
daviduche03 <daviduche4809@gmail.com>
2023-05-07 13:50:19 +0100

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 const express = require("express"),
   { urlencoded, json } = require("body-parser"),
   app = express();
-
+const BootBot = require('bootbot');
 const axios = require("axios");
 const cheerio = require("cheerio");
 require("dotenv").config();
@@ -38,157 +38,47 @@ app.get("/", function (_req, res) {
   res.send("Hello World");
 });
 
-// Adds support for GET requests to our webhook
-app.get("/webhook", (req, res) => {
-  // Your verify token. Should be a random string.
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
-  // Parse the query params
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  }
+const bot = new BootBot({
+  accessToken: process.env.PAGE_ACCESS_TOKEN,
+  verifyToken: process.env.VERIFY_TOKEN,
+  appSecret: process.env.APP_SECRET
 });
 
-// Creates the endpoint for your webhook
-app.post("/webhook", (req, res) => {
-  let body = req.body;
+bot.on('message', async (payload, chat) => {
+  const text = payload.message.text;
 
-  // Checks if this is an event from a page subscription
-  if (body.object === "page") {
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
-      // Gets the body of the webhook event
-      let webhookEvent = entry.messaging[0];
-      console.log(webhookEvent);
+  // Make a request to the OpenAI Text-Davinci API
+  const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      temperature: 0.5,
+      max_tokens: 2000,
+      top_p: 0,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop: ["{}"],
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant that generates search phrase.",
+        },
+        {
+          role: "system",
+          content:
+            "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Knowledge cutoff: {knowledge_cutoff} Current date: {current_date}",
+        },
 
-      // Get the sender PSID
-      let senderPsid = webhookEvent.sender.id;
-      console.log("Sender PSID: " + senderPsid);
-
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
-      if (webhookEvent.message) {
-        handleMessage(senderPsid, webhookEvent.message);
-      } else if (webhookEvent.postback) {
-        handlePostback(senderPsid, webhookEvent.postback);
-      }
+        { role: "user", content: `User: ${text}\nBot:` },
+      ],
     });
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send("EVENT_RECEIVED");
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
+  const aiResponse = response.data.choices[0].message.content;
+  chat.say(aiResponse);
 });
 
-// Handles messages events
-function handleMessage(senderPsid, receivedMessage) {
-  let response;
+bot.start();
 
-  // Checks if the message contains text
-  if (receivedMessage.text) {
-    // Create the payload for a basic text message, which
-    // will be added to the body of your request to the Send API
-    response = {
-      text: `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`,
-    };
-  } else if (receivedMessage.attachments) {
-    // Get the URL of the message attachment
-    let attachmentUrl = receivedMessage.attachments[0].payload.url;
-    response = {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [
-            {
-              title: "Is this the right picture?",
-              subtitle: "Tap a button to answer.",
-              image_url: attachmentUrl,
-              buttons: [
-                {
-                  type: "postback",
-                  title: "Yes!",
-                  payload: "yes",
-                },
-                {
-                  type: "postback",
-                  title: "No!",
-                  payload: "no",
-                },
-              ],
-            },
-          ],
-        },
-      },
-    };
-  }
-
-  // Send the response message
-  callSendAPI(senderPsid, response);
-}
-
-// Handles messaging_postbacks events
-function handlePostback(senderPsid, receivedPostback) {
-  let response;
-
-  // Get the payload for the postback
-  let payload = receivedPostback.payload;
-
-  // Set the response based on the postback payload
-  if (payload === "yes") {
-    response = { text: "Thanks!" };
-  } else if (payload === "no") {
-    response = { text: "Oops, try sending another image." };
-  }
-  // Send the message to acknowledge the postback
-  callSendAPI(senderPsid, response);
-}
-
-// Sends response messages via the Send API
-function callSendAPI(senderPsid, response) {
-  // The page access token we have generated in your app settings
-  const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-  const PAGE_ID = process.env.PAGE_ID || "100287699733096";
-  
-  // Construct the message body
-  axios
-    .post(
-      `https://graph.facebook.com/v16.0/${PAGE_ID}/messages?access_token=${PAGE_ACCESS_TOKEN}`,
-      {
-        recipient: {
-          id: senderPsid,
-        },
-        messaging_type: "RESPONSE",
-        message: response,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    )
-    .then(() => {
-      console.log("Message sent!");
-    })
-    .catch((error) => {
-      console.error("Unable to send message:", error);
-    });
-}
 
 let topic;
 const GetSearchPhrase = async () => {
